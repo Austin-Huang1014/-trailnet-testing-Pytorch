2c654c545ae035e660443a0ba9a1881cd6fd1520
Austin <sdnd93612@gmail.com>
2020-07-20 16:00:15 +0800

    package_upload_2020/7/20

---
+++ 2020_summer/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/austin/2020_summer/src
+# Build directory: /home/austin/2020_summer/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("deep_learning")
---
+++ 2020_summer/build/deep_learning/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/austin/2020_summer/src/deep_learning
+# Build directory: /home/austin/2020_summer/build/deep_learning
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
